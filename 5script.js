17da9fa65cc1099c3e055aff70f7a79b08fe633a
arfami096 <arfami096@gmail.com>
2026-05-24 17:58:30 +0700

    Refactor cashbackPercentage assignment logic

--- 5script.js
+++ 5script.js
@@ -5,14 +5,11 @@ let cashbackPercentage = 0;
 
 if (accountStatus === "extended") {
     cashbackPercentage = 15;
-}
-else if (accountStatus === "premium") {
+} else if (accountStatus === "premium") {
     cashbackPercentage = 20;
-}
-else if (accountStatus === "vip") {
+} else if (accountStatus === "vip") {
     cashbackPercentage = 30;
-}
-else {
+} else {
     cashbackPercentage = 10;
 }
 console.log (username + " | " + "(" + accountStatus + ")" + " аккаунт." + " Вы получаете " + cashbackPercentage + "% с покупок на бонусный счёт.")
